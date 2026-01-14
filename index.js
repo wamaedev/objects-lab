d21d2980243efcfdb161efeb4f82e5635d1b56ec
Presbury Wamae <wamaedev@gmail.com>
2026-01-14 09:11:26 +0300

    created the updateEmployeeWithKeyAndValue function and the employee object

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+const employee = {
+  name: "Sam",
+  streetAdress: "11 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value){
+  let updatedEmployee = {
+    ...object,
+    [key]: value
+  };
+  return updatedEmployee;
+}
