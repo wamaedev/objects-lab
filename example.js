e825f186241e656e789c82c8a170b96a65f5ff22
Presbury Wamae <wamaedev@gmail.com>
2026-01-20 11:17:53 +0300

    add example code snippets for reference and practice

---
+++ example.js
@@ -0,0 +1,47 @@
+// const numbers = [10, 20, 30, 40];
+
+// for(const num of numbers){
+//   console.log(num);
+// };
+
+// const word = "JavaScript";
+
+// for(const letter of word){
+//   console.log(letter);
+// }
+
+// const prices = [200, 150, 300, 50];
+
+// let total = 0;
+// for(const num of prices){
+//   total += num;
+// }
+// console.log(total);
+
+
+// const nums = [3, 7, 2, 9, 4, 10];
+
+// for(const num of nums){
+//   if(num > 5){
+//     console.log(num);
+//   };
+// };
+
+// const scores = [80, 90, 75, 88, 92];
+
+// let total = 0;
+// for(const score of scores){
+//   total += score
+// }
+// const average = total / scores.length;
+// console.log(`Average score: ${average}`);
+
+// const names = ["Alice", "Bob", "Charlie"];
+
+// for(const [index, person] of names.entries()){
+//   console.log(`Hello ${person}`);
+// }
+
+// for(const name of names){
+//   console.log(`Hello ${name}`)
+// }
